0d6aff5e350cdfa869f077194df8946d5c575e6f
MancBarr <136386687+MancBarr@users.noreply.github.com>
2023-09-20 13:28:57 -0700

    Edited lines of code and gave an introduction

--- 01_scripts/PREE.R
+++ 01_scripts/PREE.R
@@ -3,13 +3,21 @@
 
 ## Script for the PREE assignment
 
+## Mainly focused in moving the data from one of the packages available in R. We will be using 
+## the 'palmerpenguins' package.
+
+## To do the data movement we will need the following packages:
+
 library(tidyverse)
 library(palmerpenguins)
 
-## Data
+## First we will see the data available in the 'palmerpenguins' package
+
 view(penguins)
 view(penguins_raw)
 
-## Writing Data set
-write_csv(penguins,"./00_rawdata/penguins.csv")
+## Now we'll wite the dataset and added it to the environment:
+
+penguins <- write_csv(penguins,"./00_rawdata/penguins.csv")
 write_csv(penguins_raw,"./00_rawdata/penguins_raw.csv")
+
